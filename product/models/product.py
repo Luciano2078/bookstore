2c9cbb0a0eb3cf8c582543b752b127f5ca6db4b6
Luciano2078 <luciano.pereiraaristimunho@gmail.com>
2022-02-14 22:12:10 -0300

    nova2

--- product/models/product.py
+++ product/models/product.py
@@ -9,5 +9,6 @@ class Product(models.Model):
     active = models.BooleanField(default=True)
     category = models.ManyToManyField(Category, blank=True)
 
-
+    def __str__(self):
+        return self.title
 
